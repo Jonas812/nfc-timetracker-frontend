22b52067b46a5cf0937c2eea25c01b2ec1890cfa
Jonas <jonasneu812@gmail.com>
2024-10-24 23:37:11 +0200

    sync

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://timetrackerapi.grapecode.de',
+        //target: 'grapecode.de:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
